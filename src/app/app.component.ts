6a92ea6ea41fa267d74f062190c41b2af98546d3
Yuşa Kopuz <libir@outlook.com>
2021-02-20 14:26:10 +0300

    added new functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username=' ';
+  firstname = ' ';
+  lastname = ' ';
+  users= [] as any;
+
+
+  addUser(firstname: string, lastname: string) {
+    
+    this.users.push({x:firstname,y:lastname})
+    console.log(this.users)
+    this.firstname='';
+    this.lastname='';
+  }
 }
